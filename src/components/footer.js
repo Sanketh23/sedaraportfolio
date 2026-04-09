412bf87a03c57754b424b662ff89128803d2f3f7
Sanketh23 <sanketh.edara@gmail.com>
2026-04-09 18:49:44 -0400

    footer change

--- src/components/footer.js
+++ src/components/footer.js
@@ -108,10 +108,7 @@ const Footer = () => {
           href="https://github.com/bchiang7/v4"
           target="_blank"
           rel="nofollow noopener noreferrer">
-          <div>
-            Designed &amp; Built by Brittany Chiang<br></br>
-            Revised by Sanketh Edara
-          </div>
+          <div></div>
 
           {githubInfo.stars && githubInfo.forks && (
             <StyledGitHubInfo>
